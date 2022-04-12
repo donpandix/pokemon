46f67c8ece93772c4170037ef5f1c988f7041678
Cesar Gonzalez <cesar@cesarg.cl>
2022-04-11 20:43:09 -0400

    Load of the base App
    Load of the base files to develop the solution

---
+++ src/test/java/cl/cesarg/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cesarg.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
